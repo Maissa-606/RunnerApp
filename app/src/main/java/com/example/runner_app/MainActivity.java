ea5fa86cd590519894418cc791ebfd9e47296353
Maissa-606 <leibysarmiento@gmail.com>
2022-06-28 14:03:07 -0600

    Probando Repositorio

--- app/src/main/java/com/example/runner_app/MainActivity.java
+++ app/src/main/java/com/example/runner_app/MainActivity.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Probando Repositorio de GITHUB
+
+
+
     }
 }
