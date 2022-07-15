c552cbc0432fd27c4e48cdd633afb78eefc6468e
Maissa-606 <leibysarmiento@gmail.com>
2022-07-14 22:56:11 -0600

    Añadiendo Registro y Login

--- app/src/main/java/com/example/runner_app/ActivityLogin.java
+++ app/src/main/java/com/example/runner_app/ActivityLogin.java
@@ -3,8 +3,21 @@ package com.example.runner_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity
+{
+
+
+    //Declaración de Variables
+    TextView btnIngresar, btnRegistrarse;
+    EditText txtcorreo, txtcontrasenia;
+    TextView btnRecuperarClave;
+    CheckBox Recordar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
