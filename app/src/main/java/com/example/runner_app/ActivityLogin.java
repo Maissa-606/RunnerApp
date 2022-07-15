2e343635e8375ce5d24d3dbebe3d94df0fbe6d29
Maissa-606 <leibysarmiento@gmail.com>
2022-07-14 22:48:20 -0600

    Añadiendo Registro y Login

---
+++ app/src/main/java/com/example/runner_app/ActivityLogin.java
@@ -0,0 +1,14 @@
+package com.example.runner_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
